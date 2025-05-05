3461d24a816894a74cfe47a73d7fb91908d7fc00
VINOTH SUBBIAH <vinoji2005@VINOTHs-MacBook-Pro.local>
2025-05-05 09:45:59 +0400

    first project

--- environments/dev/main.tf
+++ environments/dev/main.tf
@@ -1,22 +1,3 @@
-module "network" {
-  source         = "../../modules/vnet"
-  resource_group = var.resource_group
-  location       = var.location
-  vnet_cidr      = var.vnet_cidr
-  subnet_cidr    = var.subnet_cidr
-  tags           = var.tags
-}
-
-module "aks" {
-  source           = "../../modules/aks"
-  resource_group   = var.resource_group
-  location         = var.location
-  aks_name         = var.aks_name
-  dns_prefix       = var.dns_prefix
-  subnet_id        = module.network.subnet_id
-  tags             = var.tags
-}
-
 # File: modules/vnet/main.tf
 resource "azurerm_virtual_network" "main" {
   name                = "vnet-aks"
--- main.tf
+++ main.tf
@@ -1,16 +1,16 @@
 module "network" {
-  source         = "./modules/vnet"
-  location       = var.location
+  source         = "../../modules/vnet"
   resource_group = var.resource_group
+  location       = var.location
   vnet_cidr      = var.vnet_cidr
   subnet_cidr    = var.subnet_cidr
   tags           = var.tags
 }
 
 module "aks" {
-  source           = "./modules/aks"
-  location         = var.location
+  source           = "../../modules/aks"
   resource_group   = var.resource_group
+  location         = var.location
   aks_name         = var.aks_name
   dns_prefix       = var.dns_prefix
   subnet_id        = module.network.subnet_id
