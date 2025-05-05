723d3fc637fb57c02f897e609a7af74415e2d03d
VINOTH SUBBIAH <vinoji2005@VINOTHs-MacBook-Pro.local>
2025-05-05 08:29:52 +0400

    first project

---
+++ modules/aks/outputs.tf
@@ -0,0 +1,8 @@
+output "kube_config" {
+  value     = azurerm_kubernetes_cluster.main.kube_config_raw
+  sensitive = true
+}
+
+output "aks_name" {
+  value = azurerm_kubernetes_cluster.main.name
+}
